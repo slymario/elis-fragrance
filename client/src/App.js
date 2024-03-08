f617613b5d34180c6d7f25aa7c7aaf411b32843a
slymario <nwobodo_obinna@yahoo.com>
2024-03-08 15:42:16 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,67 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from './pages/PageNotFound';
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/routes/PrivateRoute.js";
+import AdminRoute from "./components/routes/AdminRoute.js";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
+import UpdateProduct from './pages/Admin/UpdateProduct';
+import Users from './pages/Admin/Users';
+import AdminOrders from './pages/Admin/AdminOrders';
+import Orders from "./pages/user/Orders.js";
+import Profile from "./pages/user/Profile.js";
+import Products from "./pages/Admin/Products.js";
+import SearchResult from "./pages/SearchResult.js";
+import ProductDetails from "./pages/ProductDetails.js";
+import CategoriesPage from "./pages/CategoriesPage.js";
+import CategoryProduct from "./pages/CategoryProduct.js";
+import CartPage from "./pages/CartPage.js";
+
+
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/category/:slug" element={<CategoryProduct />} />
+        <Route path="/search" element={<SearchResult />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
+          <Route path="admin/orders" element={<AdminOrders />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
